f0316d0d912db376964c2b07cb857fcb4fbaf7a4
coder-hello2019 <coder.hello2019@gmail.com>
2021-10-10 18:45:56 +0200

    finished project

--- run.py
+++ run.py
@@ -1,19 +1,16 @@
 import os
 import argparse
 
-ap = argparse.ArgumentParser()
-
-args = ap.parse_args()
-
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 
 import cv2
 import numpy as np
 
 # also would be well-built as a class
 m = Sequential()
+im_height, im_width = 150, 150
 
 def load_data(img_dir):
     # grab paths of the light/dark subfolders
@@ -31,13 +28,13 @@ def load_data(img_dir):
     # flow_from_directory allows for additional data creation
     train_generator = train_datagen.flow_from_directory(
         os.path.join(img_dir, 'train/'),
-        target_size=(150, 150),
+        target_size=(im_height, im_width),
         batch_size=20,
         class_mode='binary')
 
     validation_generator = test_datagen.flow_from_directory(
         os.path.join(img_dir, 'test/'),
-        target_size=(150, 150),
+        target_size=(im_height, im_width),
         batch_size=20,
         class_mode='binary')   
 
@@ -46,14 +43,17 @@ def load_data(img_dir):
 def build_model():
 
      # our img inputs are arrays - we're making the data more friendly to the machine here; in convolution output goes into the input again
-    # N.B. don't need to specify input_shape for subsequent layers because keras infers that
-    m.add(Conv2D(64, (3, 3), input_shape=(150, 150, 3), activation='relu'))
+    # N.B. don't need to specify input_shape for subsequent layers because keras infers that - we just need to include in our input layer
+    m.add(Conv2D(64, (3, 3), input_shape=(im_height, im_width, 3), activation='relu'))
 
     # looking at maximum values in a given range
     m.add(MaxPooling2D(pool_size=(5,5)))
 
     # turning array into a single 'stream' of data
     m.add(Flatten())
+
+    # add to prevent overfitting (loss was consistently going down epoch to epoch prior to this)
+    m.add(Dropout(0.4))
 
     # 64 = units i.e. a positive int determining the dimensionality of the output space - this is a hyperparameter that is tuned during the validation stage
     m.add(Dense(32, activation='sigmoid'))
@@ -70,12 +70,34 @@ def build_model():
 def train_model(train_data, test_data):
     m.fit(
         x=train_data,
-        steps_per_epoch=1,
-        epochs=1,
+        steps_per_epoch=20,
+        epochs=10,
         validation_data = test_data,
-        validation_steps=20
+        validation_steps=20,
+        use_multiprocessing=False,
+        # question: if the workers parameter is removed, the model throws a 'not enough data' error. Nothing on this on google/SO. Investigate why this is happenning.
+        workers = 4
     )
 
+# check - might be a python version for this (v:k for k, v in d. items)
+def invert_mapping(d):
+    inverted = dict()
+    for key,value in d.items():
+        inverted[value] = key
+    return inverted
+
+def predict(img_dir):
+    # fetch images
+    img_paths = [os.path.join(img_dir, 'predict/', img_path) for img_path in os.listdir(os.path.join(img_dir, 'predict/'))]
+    
+    # cv2 read in and resize images
+    images = [cv2.imread(img) for img in img_paths]
+    images = [cv2.resize(img, (im_height, im_width)) for img in images]
+    # np reshape
+    images = [np.reshape(img, [1, im_height, im_width, 3]) for img in images]
+    # return the predictions
+
+    return [(m.predict_classes(img)[0][0], img_paths[i]) for i, img in enumerate(images)]
 
 try:
      if __name__ == '__main__':
@@ -83,6 +105,11 @@ try:
         print(f"Types of train_data and test_data: {type(train_data)}, {type(test_data)}")
         build_model()
         train_model(train_data, test_data)
+        predictions = predict('img')
+        mapping = invert_mapping(train_data.class_indices)
+
+        for val, im_name in predictions:
+            print(f'We think that {im_name} is {mapping[val]}')
 
 except KeyboardInterrupt:
     print('\nUser aborted!')
